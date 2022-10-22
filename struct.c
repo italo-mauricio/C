c1bfb6fcf03a34eb7ed10beabf9d494882cbe47a
italo <italogithub@gmail.com>
2022-10-22 18:23:05 -0300

    adicionando aula sobre struct

---
+++ struct.c
@@ -0,0 +1,21 @@
+// utilizando struct e ponteiros
+// ler e exibir informações de um estudante
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+    struct estudante
+    {
+        
+    };
+    
+
+
+
+
+    return 0;
+}
+
+
