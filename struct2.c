744608b5052a83fe81929f68e3fcaba518a53189
italo <italogithub@gmail.com>
2022-10-22 20:27:12 -0300

    trabalhando com matrizes

---
+++ struct2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+
+int main(){
+
+
+    int numero;
+    float nota;
+    char nome [40];
+
+    float notas[20]; // vetor
+    float mnotas[10][2]; // matriz
+
+
+
+
+    return 0;
+}
