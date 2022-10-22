b9173ee027585e38cde94dbb15c361b285341f7b
italo <italogithub@gmail.com>
2022-10-22 19:35:07 -0300

    update

--- struct.c
+++ struct.c
@@ -2,6 +2,7 @@
 // ler e exibir informações de um estudante
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 int main(){
@@ -22,6 +23,12 @@ int main(){
 
     ptr_stud1 = &stud1;
 
+    printf("\n Digite sua matricula: ");
+    scanf("%d", &ptr_stud1->nMatricula);
+    printf("\n Digite seu nome: ");
+    scanf("%s", &ptr_stud1->nome);
+    printf("\n Digite sua mensalidade: ");
+    scanf("%f", &ptr_stud1->nMatricula);
     printf("\n Digite sua matricula: ");
     scanf("%d", &ptr_stud1->nMatricula);
 
