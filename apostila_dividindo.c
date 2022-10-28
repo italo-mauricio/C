55632f32f724ddb3df006282bb12b40e6cd8e6ba
italo <italogithub@gmail.com>
2022-10-27 23:04:09 -0300

    aprendendo a usar a formatação de divisões

---
+++ apostila_dividindo.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main(void){
+
+    int dividendo = 10, divisor = 3;
+    float quocient = 0.0;
+
+
+    quocient = dividendo / divisor;
+    printf("%d/%d = %.2f\n", dividendo, divisor, quocient);
+    getchar();
+
+
+
+
+
+
+    return 0;
+}
