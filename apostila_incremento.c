c006358e909d19d810500ba06801d000f8cdbaaa
italo <italogithub@gmail.com>
2022-10-27 23:15:58 -0300

    aprendendo incremento e decremento

---
+++ apostila_incremento.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(void)
+{
+    int a,b,c;
+
+    a=b=c=2;
+    b = ++a;
+    c = b++;
+    printf("a:%d\nb:%d\nc:%d\n", a, b,--c);
+    getchar();
+    return 0;
+
+
+
+
+
+    return 0;
+}
