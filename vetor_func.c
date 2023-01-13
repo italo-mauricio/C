1a4bb22f9b24c268b04558ff597044a8be848f95
italo <italogithub@gmail.com>
2023-01-13 13:10:22 -0300

    update

--- vetor_func.c
+++ vetor_func.c
@@ -25,4 +25,43 @@ int main (void){
     printf("Resultado [%s]\n", texto);
 }
 
-// Exemplo com 
+// Exemplo com float
+
+int fposmax(float *vetor, int n);
+
+int main(void){
+    float vet[TAMVET];
+    int i, pos;
+
+    printf("Digite %d valores\n", TAMVET);
+    for (i=0; i<TAMVET; i++){
+        printf("%d> ", i);
+        scanf("%f", &vet[i]);
+    }
+    pos = fposmax(vet, TAMVET);
+    printf("O maior valor é %2.f e está no indice %d\n", vet[pos], pos);
+    fflush(stdin); getchar();
+    return 0;
+
+}
+
+/*
+
+    Função fposmax = Encontra a posição do maior valor em um vetor float
+    Parâmetros:
+        vetor: endereço do primeiro elemento do vetor a ser pesquisado
+        n: tamanho do vetor (número de elementos)
+    Retorno: indice do maior valor.
+        Se este valor está repetido, retorna o primeiro índice
+*/
+
+int fposmax(float *vetor, int n){
+    int posmaior = 0, i;
+
+    for (i=0; i<n; i++){
+        if (vetor [i] > vetor[posmaior]){
+            posmaior = i;
+        }
+    }
+    return (posmaior);
+}
