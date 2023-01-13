fdc842e23613c425ad819d500c019f6ad0c9176e
italo <italogithub@gmail.com>
2023-01-13 12:23:58 -0300

    update

--- vetores.c
+++ vetores.c
@@ -19,5 +19,32 @@ int main(void){
         {9993, "afonso", {18,12,1995}},
         {9994, "pablo", {15,11,1995}}
     };
-};
+
+
+int i, achou, m;
+char escolha;
+
+do{
+    printf("Matricula: ");
+    scanf("%d", &m);
+    achou = 0;
+    for (i = 0; i < TAM_VET; i++){
+        if (turma[i].matricula == m){
+            printf("Nome: %s\n", turma[i].nome);
+            printf("Nascimento: %02d/%02d/%04d\n",
+                turma[i].nascimento.dia,
+                turma[i].nascimento.mes,
+                turma[i].nascimento.ano);
+            achou = 1;
+            break;
+        }
+    }
+    if(achou == 0)
+    printf("Matricula não encontrada!\n");
+printf("\nContinuar? [S/N]: ");
+scanf(" %c", &escolha);
+}while(escolha == 's' || escolha == 'S');
+return 0;
+}
+
 
